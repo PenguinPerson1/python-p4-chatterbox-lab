946d98b537335add5005401ecaae4941337f5890
Annania Plossl <nia.plossl@gmail.com>
2024-05-15 13:17:08 -0400

    completed the lab

--- server/app.py
+++ server/app.py
@@ -14,13 +14,40 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.get('/messages')
 def messages():
-    return ''
+    return [ m.to_dict() for m in Message.query.all() ], 200
 
-@app.route('/messages/<int:id>')
+@app.post('/messages')
+def add_message():
+    new_message = Message(
+        body = request.json.get('body'),
+        username = request.json.get('username')
+        )
+    db.session.add(new_message)
+    db.session.commit()
+    return new_message.to_dict()
+
+@app.patch('/messages/<int:id>')
 def messages_by_id(id):
-    return ''
+    mess = Message.query.where(Message.id == id).first()
+    if mess:
+        for key in request.json.keys():
+            setattr(mess,key,request.json[key])
+        db.session.add(mess)
+        db.session.commit()
+        return mess.to_dict()
+    return {message: "message not found."}, 404
+
+
+@app.delete('/messages/<int:id>')
+def delete_message(id):
+    mess = Message.query.where(Message.id == id).first()
+    if mess:
+        db.session.delete(mess)
+        return {}, 204
+    else:
+        return { 'error': 'Not found' }, 404
 
 if __name__ == '__main__':
     app.run(port=5555)
